41061f6352aaed6c62191a22bcfe046a5fbf83cf
Prashant Cholachagudda <pvc@outlook.com>
2014-05-20 15:44:30 +0530

    Update index page contents

--- server.js
+++ server.js
@@ -1,21 +1,14 @@
-// var http = require('http');
-// http.createServer(function(req, res){
-//     res.writeHead(200, {'Content-Type':'text/plain'});
-//     res.end('Hello world');
-// }).listen(3000, '127.0.0.1');
-// console.log('Server running at http://127.0.0.1');
-
 var express = require('express'),
     path = require('path'),
     http = require('http');
 
 var app = express();
 
-app.configure(funtion () {
+app.configure(function () {
     app.set('port', process.env.PORT || 3000);
-    app.use(express.logger('dev'));
+    app.use(express.logger('dev'));  /* 'default', 'short', 'tiny', 'dev' */
     app.use(express.bodyParser()),
-    app.use(express.static(path.join(__dirname, 'piblic')));
+    app.use(express.static(path.join(__dirname, 'public')));
 });
 
 http.createServer(app).listen(app.get('port'), function(){
